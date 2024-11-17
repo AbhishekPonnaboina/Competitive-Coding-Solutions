b71b7d5f917e31a2238701fe8f6fb5d3c7262398
P Abhishek <abhi04457@Gmail.com>
2024-11-17 22:42:17 +0530

    Time: 2 ms (97.64%), Space: 17.5 MB (90.42%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,6 +1,30 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        resa = []
+        #dp solution
+
+        dp = [0]*(n+1)
+        offset = 1
+
+        for i in range(1,n+1):
+            if offset*2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        """resa = []
 
         for i in range(n+1):
             res = 0
@@ -8,4 +32,4 @@ class Solution:
                 res += (i & 1)
                 i = i // 2
             resa.append(res)
-        return resa
+        return resa"""

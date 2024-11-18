dc4b5fba62329ca1c8867fe94d81913efd206103
P Abhishek <abhi04457@Gmail.com>
2024-11-18 17:30:28 +0530

    Time: 11 ms (87.53%), Space: 17 MB (5.54%) - LeetHub

---
+++ 3540-hash-divided-string/3540-hash-divided-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def stringHash(self, s: str, k: int) -> str:
+        #myhash = dict()
+        ord1 = 97
+        """for i in range(26):
+            myhash[chr(ord1)] = i
+            ord1 += 1
+        print(myhash)"""
+        res1 = ""
+        res = []
+        for i in range(0,len(s),k):
+            sub = s[i : i+k]
+            res.append(sub)
+        for i in res:
+            sum1 = 0
+            for ele in i:
+                sum1 += ord(ele) - 97
+            sum1 = sum1 % 26
+            res1 += chr(97 + sum1) 
+
+        return res1
+
+
+        
+        
+        
+        
+        
+        return s
+
+
+        

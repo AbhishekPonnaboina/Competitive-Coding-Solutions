1be980fa97aa172417d90e7fd89f2bafc40c87bc
P Abhishek <abhi04457@Gmail.com>
2024-11-23 23:22:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.py
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.py
@@ -8,16 +8,16 @@ class Solution:
         arr.sort()
 
 
-        max_diff = arr[-1] - arr[0]
+        diff = arr[-1] - arr[0]
     
         for i in range(n - 1):
-            max_height = max(arr[i] + k, arr[-1] - k)
+            maxht = max(arr[i] + k, arr[-1] - k)
         
-            min_height = min(arr[0] + k, arr[i + 1] - k)
+            minht = min(arr[0] + k, arr[i + 1] - k)
     
-            max_diff = min(max_diff, max_height - min_height)
+            diff = min(diff, maxht - minht)
     
-        return max_diff
+        return diff
 
 
 #{ 

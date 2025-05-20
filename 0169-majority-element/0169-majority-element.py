dddb2b1ad393c02ad6858d23bd366e5ec96677c9
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-05-20 13:35:18 +0530

    Time: 7 ms (57.57%), Space: 19.4 MB (19.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,26 @@
 from collections import Counter 
 class Solution:
     def majorityElement(self, arr: List[int]) -> int:
-        cou = Counter(arr)
+        #moore voting algorithm
+        res = 0
+        count = 1
+        for i in range(1,len(arr)):
+            if arr[res] == arr[i]:
+                count += 1
+            else:
+                count -= 1
+            
+            if count == 0:
+                res = i
+                count = 1
+        return arr[res]
+
+
+
+
+
+
+        """cou = Counter(arr)
         res = 0
         
         
@@ -9,4 +28,4 @@ class Solution:
             if i > len(arr)//2:
                 res = key
                 
-        return res
+        return res"""

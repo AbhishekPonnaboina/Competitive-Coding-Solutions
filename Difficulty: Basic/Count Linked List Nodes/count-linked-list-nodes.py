37d099a0cdd0cd985476c8a8af7421c22b0d839b
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-05-29 22:20:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.py
@@ -0,0 +1,19 @@
+'''
+
+#Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        '''
+class Solution:
+    # Function to count nodes of a linked list.
+    def getCount(self, head):
+        # code here
+        cnt = 0
+        
+        while head:
+            head = head.next
+            cnt += 1
+        
+        return cnt

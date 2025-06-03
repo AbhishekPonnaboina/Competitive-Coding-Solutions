f0796abe5a50a76c9aa57822f4c8593f5c3b231f
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-06-03 21:19:02 +0530

    Time: 7 ms (19.03%), Space: 18.5 MB (52.74%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,7 +3,36 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        col0 = 1
         nr = len(matrix)
+        nc = len(matrix[0])
+
+        for i in range(nr):
+            for j in range(nc):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0 
+
+        for i in range(1,nr):
+            for j in range(1,nc):
+                if matrix[i][j] != 0:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for j in range(nc):
+                matrix[0][j] = 0
+            
+        if col0 == 0:
+            for i in range(nr):
+                matrix[i][0] = 0
+            
+
+
+        """nr = len(matrix)
         nc = len(matrix[0])
         rowidx = [0 for i in range(nr)]
         colidx = [0 for i in range(nc)]
@@ -17,7 +46,7 @@ class Solution:
         for i in range(nr):
             for j in range(nc):
                 if rowidx[i] == 1 or colidx[j] == 1:
-                    matrix[i][j] = 0
+                    matrix[i][j] = 0"""
             
         
                                      

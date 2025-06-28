f8f1fb5ad7c231ab594e544ea80a95d843b2d9c0
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-06-28 20:04:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.py
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.py
@@ -3,7 +3,7 @@
 """
 # Node Class
 
-class Node:
+class node:
     def __init__(self, val):
         self.data = val
         self.next = None
@@ -14,67 +14,16 @@ class Solution:
     #Function to reverse a linked list.
     def reverseList(self, head):
         # Code here
-        if head is None:
-            return head
+        stack = []
         curr = head
-        prev = None
-        
-        while curr is not None:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        return prev
-        
+        while curr:
+            stack.append(curr.data)
+            curr = curr.next
+        curr = head
+        while curr:
+            curr.data = stack.pop()
+            curr = curr.next
+        return head
 
 
 
-
-#{ 
- # Driver Code Starts
-# Node Class
-class Node:
-
-    def __init__(self, val):
-        self.data = val
-        self.next = None
-
-
-# Linked List Class
-class Linked_List:
-
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def insert(self, val):
-        if self.head is None:
-            self.head = Node(val)
-            self.tail = self.head
-        else:
-            self.tail.next = Node(val)
-            self.tail = self.tail.next
-
-
-def printList(head):
-    tmp = head
-    while tmp:
-        print(tmp.data, end=' ')
-        tmp = tmp.next
-    print()
-
-
-if __name__ == '__main__':
-    for i in range(int(input())):
-
-        arr = [int(x) for x in input().split()]
-
-        lis = Linked_List()
-        for i in arr:
-            lis.insert(i)
-
-        newHead = Solution().reverseList(lis.head)
-        printList(newHead)
-        print("~")
-
-# } Driver Code Ends

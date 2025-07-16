3bc3e29d9857b7513ff64b970b8ba7d10da22a79
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-07-16 20:48:34 +0530

    Time: 1 ms (3.4%), Space: 17.8 MB (54.76%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -7,13 +7,65 @@
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         
-        res = []
         
-        def postorder(root,res):
-            if not root:
-                return 
-            postorder(root.left,res)
-            postorder(root.right,res)
-            res.append(root.val)
-        postorder(root,res)
+        curr = root
+
+        res = []
+        stack = []
+
+        while stack or curr:
+            if curr is not None:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                temp = stack[-1].right
+                if temp is None:
+                    temp = stack.pop()
+                    res.append(temp.val)
+                    
+
+                    while stack and temp == stack[-1].right:
+                        temp = stack.pop()
+                        res.append(temp.val)
+
+                else:
+                    curr = temp 
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        """
+        using two stacks
+        if not root:
+            return []
+
+        stack1 = [root]
+        stack2 = []
+
+        while stack1:
+            ele = stack1.pop()
+            stack2.append(ele.val)
+            if ele.left:
+                stack1.append(ele.left)
+            if ele.right:
+                stack1.append(ele.right)
+            
+        
+        return stack2[::-1]
+        """
+                

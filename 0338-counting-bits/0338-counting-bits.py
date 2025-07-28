b0f2e4ba0734cf702a0f96d7dd5e68a0438c3ea9
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-07-28 12:47:11 +0530

    Time: 11 ms (40.36%), Space: 18.3 MB (98.47%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,12 +1,22 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         
+        dparr = [0]*256
+
+        for i in range(256):
+            # print(i&1)
+            dparr[i] = (i & 1) + dparr[i>>1]
+        
         resArr = []*(n+1)
 
         for i in range(n+1):
-            res = 0
-            while i:
-                res += 1
-                i = i & i - 1
-            resArr.append(res)
+            resArr.append(
+                dparr[i & 0xff] +
+                dparr[i>>8 & 0xff] +
+                dparr[i>>16 & 0xff] +
+                dparr[i>>24 & 0xff]
+            )
         return resArr
+        
+
+        

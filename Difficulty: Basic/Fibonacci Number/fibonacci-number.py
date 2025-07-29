0611fbe04cfde53f5e6391e4657c726dcb79295f
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-07-30 00:03:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fibonacci Number/fibonacci-number.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+#Back-end complete function Template for Python 3
+n = int(input())
+import math
+########### Write your code below ###############
+fib = round((((1+math.sqrt(5))/2)**n)/math.sqrt(5))
+
+########### Write your code above ###############
+print(fib)

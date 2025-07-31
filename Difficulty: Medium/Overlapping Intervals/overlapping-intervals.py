d6ccadbc7587b585244ba2274e167a1718691dd1
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-08-01 00:54:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
@@ -1,38 +1,16 @@
 class Solution:
 	def mergeOverlap(self, arr):
-		#Code here
-        arr.sort(key=lambda x: x[0])
-        merged = []
-        for interval in arr:
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
-            else:
-                merged[-1][1] = max(merged[-1][1], interval[1])
-        
-        return merged
-
-
-#{ 
- # Driver Code Starts
-if __name__ == '__main__':
-    T = int(input())
-    for i in range(T):
-        n = int(input())
-        # a = list(map(int, input().strip().split()))
-        arr = []
-        # j = 0
-        for i in range(n):
-            a = list(map(int, input().strip().split()))
-            x = a[0]
-            # j += 1
-            y = a[1]
-            # j += 1
-            arr.append([x, y])
-        obj = Solution()
-        ans = obj.mergeOverlap(arr)
-        for i in ans:
-            for j in i:
-                print(j, end=" ")
-        print()
-
-# } Driver Code Ends
+		# Code here
+		
+		arr.sort()
+		res = []
+		
+		for i in range(len(arr)):
+		    if len(res) == 0:
+		        res.append(arr[i])
+		    if arr[i][0] > res[-1][1]:
+		        res.append(arr[i])
+		    else:
+		        maxi = max(arr[i][1],res[-1][1])
+		        res[-1][1] = maxi
+	    return res

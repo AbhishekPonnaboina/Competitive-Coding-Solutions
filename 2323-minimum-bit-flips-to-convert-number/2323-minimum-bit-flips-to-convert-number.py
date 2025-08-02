57a27a9d88f85147189d01086a43373a94a1a0e8
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-08-02 12:17:23 +0530

    Time: 0 ms (100%), Space: 17.6 MB (83.32%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -1,6 +1,15 @@
 class Solution:
     def minBitFlips(self, start: int, end: int) -> int:
-        cnt = 0
+
+        myxor = start ^ end
+        cnt =0
+        while myxor:
+            #brian neighmans algorithm
+            myxor = myxor & (myxor - 1)
+            cnt += 1
+        return cnt    
+
+"""        cnt = 0
         while start != end :
             n = start & 1
             print("n=",n)
@@ -9,6 +18,6 @@ class Solution:
             print("p=",p)
             end = end >> 1
             if n != p:
-                cnt += 1
-        return cnt
+                cnt += 1"""
+        
         

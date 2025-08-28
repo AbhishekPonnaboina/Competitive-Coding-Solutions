ef451b9d10fee27b40b106372a7cd45801f889e4
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-08-28 12:00:59 +0530

    Time: 181 ms (61.58%), Space: 23.7 MB (14.25%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,41 @@
+class Solution:
+    def totalFruit(self, nums: List[int]) -> int:
+        r,l = 0,0
+        n = len(nums)
+        res = 0
+        hashmap = defaultdict(int)
+
+        for r in range(n):
+            hashmap[nums[r]] += 1
+            # print("Hashmap before=",hashmap)
+            while len(hashmap) > 2:
+                hashmap[nums[l]] -= 1
+                if hashmap[nums[l]] == 0:
+                    del hashmap[nums[l]]
+                l += 1
+            # print("Hashmap After=",hashmap)
+            res = max(res,r-l+1)
+        return res   
+
+
+  
+
+
+
+        #wrote the brute force but it didnot work sad tle
+        # r,l = 0,0
+        # n = len(nums)
+        
+        # res = 0
+
+        # for i in range(n):
+        #     hashmap = defaultdict(int)
+            
+        #     for j in range(i,n):
+        #         hashmap[nums[j]] += 1
+        #         # print("hashmap",hashmap)
+        #         if len(hashmap) > 2:
+        #             break
+        #         res = max(res,j-i+1)
+        #     # print("------------------------")
+        # return res

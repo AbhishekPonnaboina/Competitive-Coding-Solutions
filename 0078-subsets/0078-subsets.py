7f4db916b206809d55c1d0ad8109f6de640da095
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-09-10 15:15:12 +0530

    Time: 4 ms (5.04%), Space: 18.1 MB (21.75%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,38 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        subsets = 2 ** len(nums)
-        res = [[]for _ in range(subsets)]
+        n = len(nums)
+        res = []
+        subset = []
+        #here we need to do the simple at each step do we add the element or not 
+        #like include the element or not include the element thats it 
+        def BackTracking(i):
+            if i >= n:
+                print(subset)
+                res.append(subset.copy())
+                return
+            #incluind the current index element
+            subset.append(nums[i])
+            BackTracking(i+1)
+
+            #Here not including the element 
+            #so we just need to remove the element we added
+            subset.pop()
+            BackTracking(i+1)
+        
+        BackTracking(0)
+        return res
+
+        #Bit manipulation approach
+        # subsets = 2 ** len(nums)
+        # res = [[]for _ in range(subsets)]
         
 
-        for i in range(0,subsets):
-            vala = []
-            for j in range(len(nums)):
-                x = i & (1 << j)
-                if x:
-                    vala.append(nums[j])
-            res[i] = vala
+        # for i in range(0,subsets):
+        #     vala = []
+        #     for j in range(len(nums)):
+        #         x = i & (1 << j)
+        #         if x:
+        #             vala.append(nums[j])
+        #     res[i] = vala
         
-        return res
-        
+        # return res

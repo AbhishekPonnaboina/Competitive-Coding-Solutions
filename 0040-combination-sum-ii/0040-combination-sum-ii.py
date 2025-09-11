190412062d8e1ea0bdda46813aa021e74bb5d205
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-09-11 17:00:03 +0530

    Time: 31 ms (24.67%), Space: 17.9 MB (36.16%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -15,6 +15,9 @@ class Solution:
             sub.append(arr[idx])
             generatesub(arr,sub,res,idx+1,suma-arr[idx])
             sub.pop()
+
+            # same problem but we need to make slight changes like not inlcuding the same number while not including in the recursion thats what we need to change here thats it
+            # we can also change this to binary search for the optimization 
             for j in range(idx+1,n):
                 if arr[j] != arr[idx]:
                     generatesub(arr,sub,res,j,suma)

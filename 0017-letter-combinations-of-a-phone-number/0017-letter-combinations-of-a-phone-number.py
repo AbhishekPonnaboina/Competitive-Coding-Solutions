d7ef6aba76aca6a455df670cce28992b90d00b1a
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-09-13 16:17:00 +0530

    Time: 0 ms (100%), Space: 17.7 MB (69.56%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,14 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        #when using strings immutable data type dont try to backtrack manually
+        #we just need not do that python creates a new object
+        #lesson learned on trying to change an immutable data type 
+        #pass by reference-to-immutable-object. that means creating an 
+        # new string and passing the value to in the recdursion call itself
+        #so we no need to change that particular backtracking
 
         if not digits:
             return []
-
         n = len(digits)
         
         dial = [[],[], ['a', 'b', 'c'],       
@@ -16,7 +21,6 @@ class Solution:
                     ['w', 'x', 'y', 'z'],   
                 ]
         sub = ""
-
         res = []
 
         def generatesub(idx,sub):
@@ -29,6 +33,6 @@ class Solution:
             sublist = int(digits[idx]) #2
             for i in range(len(dial[sublist])):
                 generatesub(idx+1,sub+dial[sublist][i])
-        
+
         generatesub(0,sub) ## 2
         return res

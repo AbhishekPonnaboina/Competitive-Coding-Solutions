012c9404b90960db7f3d9f32f44217dfcc216ea7
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-09-13 14:36:54 +0530

    Time: 1 ms (34.4%), Space: 17.7 MB (86.74%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum3(self, k: int, suma: int) -> List[List[int]]:
+        
+        res = []
+        sub = []
+
+        def generatesub(suma,idx):
+            if suma == 0 and len(sub) == k :          
+                res.append(sub.copy())
+                return
+            if suma < 0 or len(sub) > k or idx == 9:
+                return
+            sub.append(idx+1)
+            generatesub(suma-(idx+1),idx+1)
+            sub.pop()
+            generatesub(suma,idx+1)
+        
+        generatesub(suma,0)
+        return res
+

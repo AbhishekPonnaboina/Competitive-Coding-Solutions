66240aa9839845ec2ae358efff5dedd981dad275
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-10-18 23:07:32 +0530

    Time: 45 ms (67.4%), Space: 19.6 MB (53.75%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -7,34 +7,52 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
-            return 
-
-
-        myhash = dict()
-
+            return None
         slow = head
         fast = head
 
-        while fast.next and fast.next.next:
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow is fast:
-                myhash[slow] = True
-                slow = slow.next
-
+                #i wrote the proof in the book you can check it there
+                slow = head
                 while slow is not fast:
-                    myhash[slow] = True
                     slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
+
+        # so this is O(n) memory bro
+        # if not head:
+        #     return 
+
+
+        # myhash = dict()
+
+        # slow = head
+        # fast = head
+
+        # while fast.next and fast.next.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        #     if slow is fast:
+        #         myhash[slow] = True
+        #         slow = slow.next
+
+        #         while slow is not fast:
+        #             myhash[slow] = True
+        #             slow = slow.next
                 
-                break
+        #         break
 
         
-        slow = head 
+        # slow = head 
 
-        while slow:
-            if slow in myhash:
-                return slow
-            slow = slow.next
+        # while slow:
+        #     if slow in myhash:
+        #         return slow
+        #     slow = slow.next
     
         
 

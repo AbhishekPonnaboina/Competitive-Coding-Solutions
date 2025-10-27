d7d2bff1453686c082d0adcd042880d5b47afcef
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-10-27 12:40:03 +0530

    Time: 52 ms (5.3%), Space: 20.4 MB (71.68%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -4,17 +4,33 @@ class Solution:
         n = len(arr)
         hashmap = defaultdict(int)
         hashmap[0] = 1
+
+        res = 0
         psum = 0
-        count = 0
 
         for i in range(n):
             psum += arr[i]
-            if (psum-k) in hashmap.keys():
-                count += hashmap[psum-k]
+            if psum - k in hashmap.keys():
+                res += hashmap[psum-k]
             hashmap[psum] += 1
+        
+        return res
+
+
+        # n = len(arr)
+        # hashmap = defaultdict(int)
+        # hashmap[0] = 1
+        # psum = 0
+        # count = 0
+
+        # for i in range(n):
+        #     psum += arr[i]
+        #     if (psum-k) in hashmap.keys():
+        #         count += hashmap[psum-k]
+        #     hashmap[psum] += 1
 
 
                 
-        return count 
+        # return count 
 
         

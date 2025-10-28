60d8f6ea7c0b032f555c8d5f78a17f0bc85191e5
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-10-28 15:12:53 +0530

    Time: 37 ms (43.73%), Space: 34.9 MB (62.64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,16 +1,54 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        curr = head
+        #follow up is how can we do it without extra space?
+        #finding the middle part
+        #revrsing 
+        #checking for the palindrom
+        #before returning the ans we make sure to reverse what we changed
+        def reversehelpie(head):
+            prev = None
+            curr = head
 
-        while curr:
-            stack.append(curr.val)
-            curr = curr.next
-        
-        curr = head
+            while curr:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+            return prev
 
-        for i,val in enumerate(reversed(stack)):
-            if val != curr.val:
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = reversehelpie(slow.next)
+        start = head
+
+        while mid:
+            if start.val != mid.val:
+                reversehelpie(slow.next)
                 return False
-            curr = curr.next
-        return True
+            start = start.next
+            mid = mid.next
+        else:
+            reversehelpie(slow.next)
+            return True
+        
+
+        #basic approach my nig
+        # stack = []
+        # curr = head
+
+        # while curr:
+        #     stack.append(curr.val)
+        #     curr = curr.next
+        
+        # curr = head
+
+        # for i,val in enumerate(reversed(stack)):
+        #     if val != curr.val:
+        #         return False
+        #     curr = curr.next
+        # return True

427b57bfbd2ad375d703875903a85a278ac25767
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-10-28 14:17:02 +0530

    Time: 31 ms (62.5%), Space: 39.4 MB (27.12%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,7 +1,3 @@
-
-
-
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         stack = []
@@ -12,13 +8,9 @@ class Solution:
             curr = curr.next
         
         curr = head
-        # print(stack)
 
-        while curr:
-            if stack.pop() != curr.val:
+        for i,val in enumerate(reversed(stack)):
+            if val != curr.val:
                 return False
             curr = curr.next
-        
         return True
-
-

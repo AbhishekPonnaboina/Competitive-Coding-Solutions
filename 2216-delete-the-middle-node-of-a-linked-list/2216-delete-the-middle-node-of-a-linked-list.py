59beb76f4e02a309bb4601a2e96800aa4f7a16b2
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-11-05 12:56:24 +0530

    Time: 43 ms (90.83%), Space: 48.4 MB (94.58%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
@@ -5,20 +5,44 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #fast and slow pointer approach
+        #tortoise and hoares method will identify the middle element
+        #but we dont need the middle 
+        #we need middle - 1
+        # thats why we add an duplicate ele and then 
+        #point from there
+        if not head or not head.next:
+            return 
+        dummy = ListNode()
+        dummy.next = head
+        slow = dummy
+
+        fast = head 
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        slow.next = slow.next.next
+        
+        return head
+
+
+
         #basic approach
 
-        if not head or not head.next:
-            return
+        # if not head or not head.next:
+        #     return
         
-        curr = head
-        lena = 0
+        # curr = head
+        # lena = 0
 
-        while curr:
-            curr = curr.next
-            lena += 1
-        curr = head 
-        for i in range(lena//2 - 1):
-            curr = curr.next
+        # while curr:
+        #     curr = curr.next
+        #     lena += 1
+        # curr = head 
+        # for i in range(lena//2 - 1):
+        #     curr = curr.next
         
-        curr.next = curr.next.next
-        return head
+        # curr.next = curr.next.next
+        # return head

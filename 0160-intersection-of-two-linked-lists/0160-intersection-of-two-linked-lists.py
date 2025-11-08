7a9120fb9da260b1fb9f402c52be245c4ea54e84
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-11-08 14:32:53 +0530

    Time: 94 ms (13.54%), Space: 27.9 MB (47.53%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -5,42 +5,75 @@
 #         self.next = None
 
 class Solution:
-    def helper(self,dist,small,large):
-        """ this is for better approach which takes o(n) + o(2m)"""
-        curr1 = large
-        for i in range(dist):
-            curr1 = curr1.next
+    # def helper(self,dist,small,large):
+    #     """ this is for better approach which takes o(n) + o(2m)"""
+    #     curr1 = large
+    #     for i in range(dist):
+    #         curr1 = curr1.next
         
-        curr2 = small
+    #     curr2 = small
 
-        while curr1 and curr2 and curr1 != curr2:
-            curr1 = curr1.next
-            curr2 = curr2.next
+    #     while curr1 and curr2 and curr1 != curr2:
+    #         curr1 = curr1.next
+    #         curr2 = curr2.next
         
-        if curr1:
-            return curr1
-        return 
+    #     if curr1:
+    #         return curr1
+    #     return 
 
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        #proof in the book nig
         if not headA or not headB:return None
-
-        l1 = l2 = 0
 
         curr1 = headA
         curr2 = headB
+        
 
-        while curr1:
-            l1 += 1
+        while curr1 != curr2:
             curr1 = curr1.next
-        
-        while curr2:
             curr2 = curr2.next
-            l2 += 1
+
+            if curr1 == curr2:
+                return curr1
+            
+            if not curr1:
+                curr1 = headB
+            if not curr2:
+                curr2 = headA
+                
+        return curr1
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #this is kinda of better appraoch compared to what we
+        #did like not using extra space
+        # l1 = l2 = 0
+
+        # curr1 = headA
+        # curr2 = headB
+
+        # while curr1:
+        #     l1 += 1
+        #     curr1 = curr1.next
         
-        if l1 > l2:
-            return self.helper(l1-l2,headB,headA)
-        else:
-            return self.helper(l2-l1,headA,headB)
+        # while curr2:
+        #     curr2 = curr2.next
+        #     l2 += 1
+        
+        # if l1 > l2:
+        #     return self.helper(l1-l2,headB,headA)
+        # else:
+        #     return self.helper(l2-l1,headA,headB)
 
 
 

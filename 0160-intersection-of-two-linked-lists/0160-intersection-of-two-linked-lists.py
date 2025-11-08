82038cfbed61aa0a1908d3cb613e1f44ad67a6e9
Abhishek Ponnaboina <abhi04457@Gmail.com>
2025-11-08 14:04:17 +0530

    Time: 82 ms (71.37%), Space: 27.8 MB (74.27%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -5,21 +5,77 @@
 #         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    def helper(self,dist,small,large):
+        """ this is for better approach which takes o(n) + o(2m)"""
+        curr1 = large
+        for i in range(dist):
+            curr1 = curr1.next
         
-        pa = headA
-        pb = headB
+        curr2 = small
 
-        while pa != pb:
-            if pa:
-                pa = pa.next
-            else:
-                pa = headB
+        while curr1 and curr2 and curr1 != curr2:
+            curr1 = curr1.next
+            curr2 = curr2.next
+        
+        if curr1:
+            return curr1
+        return 
+
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:return None
+
+        l1 = l2 = 0
+
+        curr1 = headA
+        curr2 = headB
+
+        while curr1:
+            l1 += 1
+            curr1 = curr1.next
+        
+        while curr2:
+            curr2 = curr2.next
+            l2 += 1
+        
+        if l1 > l2:
+            return self.helper(l1-l2,headB,headA)
+        else:
+            return self.helper(l2-l1,headA,headB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #lala this is not working,extra space also
+        # seta = set()
+        # curr1 = headA
+        # curr2 = headB
+
+        # while curr1 or curr2:
+        #     # print(seta)
+        #     if curr1 and curr1 in seta:
+        #         return curr1
+        #     if curr2 and curr2 in seta:
+        #         return curr2
             
-            if pb:
-                pb = pb.next
-            else:
-                pb = headA
             
-        return pa
-            
+        #     if curr1:
+        #         seta.add(curr1)
+        #         curr1 = curr1.next
+        #     if curr2:
+        #         seta.add(curr2)
+        #         curr2 = curr2.next
+        
+        # return
+
+
+
+    
